90e61fe58b54b2280b8df993955de12a668b5090
MarissaAG <747121@student.dusd.net>
2022-11-28 09:56:53 -0800

    world interactions
    Added world interactions and i=decorations to Ruby's Adventure

--- RubysAdventureGutierrez/Assets/Scripts/RubyControlller.cs
+++ RubysAdventureGutierrez/Assets/Scripts/RubyControlller.cs
@@ -4,27 +4,34 @@ using UnityEngine;
 
 public class RubyControlller : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       //Create 2 variables to use the unity built in axes
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        //Create 2 variables to use the unity built in axes
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
 
         //Create our movement vector
-        Vector2 position = transform.position;
+        Vector2 position = rigidbody2d.position;
 
         //Create horizontal and vertical movement
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
-        //Setting new position
-        transform.position = position;
+        rigidbody2d.MovePosition(position);
     }
 }
